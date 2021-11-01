dfe3a7009b55d4591d36bc3455908ea934c54694
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-11-01 15:11:54 +0700

    update api key

--- lib/api/provider/api_news_provider.dart
+++ lib/api/provider/api_news_provider.dart
@@ -6,14 +6,13 @@ class ApiNewsProvider {
   final String _baseUrl = "https://newsapi.org";
 
   //Enter your api key here
-  final String _apiNewsKey = "94a764d3b92a4c728deb8f579ec1164d";
+  final String _apiNewsKey = "";
 
   Future<List<News>> getNews() async {
     final Response response = await _dio
         .get('$_baseUrl/v2/everything?domains=wsj.com&apiKey=$_apiNewsKey');
     if (response.statusCode == 200) {
       List<News> news;
-
       news = (response.data['articles'] as List)
           .map((i) => News.fromJson(i))
           .toList();
